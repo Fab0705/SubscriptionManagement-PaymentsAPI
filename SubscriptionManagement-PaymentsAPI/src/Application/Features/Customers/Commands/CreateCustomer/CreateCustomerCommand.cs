62979800a3a338380411b92781a6096fd48e908b
Fab0705 <117318115+Fab0705@users.noreply.github.com>
2026-03-19 12:19:34 -0500

    Adding Password value and User Identity Register

--- SubscriptionManagement-PaymentsAPI/src/Application/Features/Customers/Commands/CreateCustomer/CreateCustomerCommand.cs
+++ SubscriptionManagement-PaymentsAPI/src/Application/Features/Customers/Commands/CreateCustomer/CreateCustomerCommand.cs
@@ -1,27 +1,41 @@
-﻿using SubscriptionManagement_PaymentsAPI.Application.Common.Interfaces;
+﻿using FluentValidation.Results;
+using SubscriptionManagement_PaymentsAPI.Application.Common.Interfaces;
 using SubscriptionManagement_PaymentsAPI.Domain.Entities;
+using ValidationException = SubscriptionManagement_PaymentsAPI.Application.Common.Exceptions.ValidationException;
 
 namespace SubscriptionManagement_PaymentsAPI.Application.Features.Customers.Commands.CreateCustomer;
 
 public record CreateCustomerCommand : IRequest<Guid>
 {
     public string Email { get; init; } = null!;
+    public string Password { get; init; } = null!;
     public string? StripeCustomerId { get; init; }
 }
 
 public class CreateCustomerCommandHandler : IRequestHandler<CreateCustomerCommand, Guid>
 {
     private readonly IApplicationDbContext _context;
-    public CreateCustomerCommandHandler(IApplicationDbContext context)
+    private readonly IIdentityService _identityService;
+    public CreateCustomerCommandHandler(IApplicationDbContext context, IIdentityService identityService)
     {
         _context = context;
+        _identityService = identityService;
     }
     public async Task<Guid> Handle(CreateCustomerCommand request, CancellationToken cancellationToken)
     {
-        var entity = new Customer
+        var (result, userId) = await _identityService.CreateUserAsync(request.Email, request.Password);
+
+        if (!result.Succeeded)
+        {
+            var failures = result.Errors.Select(e => new ValidationFailure("Identity", e));
+            throw new ValidationException(failures);
+        }
+
+            var entity = new Customer
         {
             Email = request.Email,
-            StripeCustomerId = request.StripeCustomerId
+                ApplicationUserId = Guid.Parse(userId.ToString()),
+                StripeCustomerId = request.StripeCustomerId
         };
         _context.Customers.Add(entity);
         await _context.SaveChangesAsync(cancellationToken);
